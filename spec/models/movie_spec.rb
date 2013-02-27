914d81cca34bdf087493d32abc8ec9d4f02e2efb
bon27 <jingkai64@gmail.com>
2013-02-27 16:17:33 +0800

    update the movie_spec.rb in spec document to use factory girl

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,8 +1,12 @@
 describe Movie do
   fixtures :movies
   it 'should include rating and year in full name' do
-    movie = movies(:milk_movie)
+    #movie = movies(:milk_movie)
+    #movie.name_with_rating.should == 'Milk(R)'
+  # changed to factory from fixture usage
+    movie = FactoryGirl.build(:movie, :title=>'Milk', :rating=>'R')
     movie.name_with_rating.should == 'Milk(R)'
+  
   end
 end
 
